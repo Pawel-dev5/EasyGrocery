c05f77ffe0ec64123c5547f097b8a4480c72b74d
Pawel-dev5 <p.nowecki@gmail.com>
2022-10-25 02:16:55 +0200

    install packages

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+				alias: {
+					'*': '.',
+					root: './',
+					src: './src',
+					components: './src/components',
+					utils: './src/utils',
+					assets: './src/assets',
+					coreUI: './src/coreUI',
+				},
+			},
+		],
+		['react-native-reanimated/plugin'],
+	],
 };
